f342a89a86db4ea1bb2dae0eeda0318a953eb74f
Florent <florent@frizoncorrea.fr>
2025-01-29 21:54:54 +0100

    deal with esp32_c3

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>   // needed for PlatformIO
 #include <Mesh.h>
-#include <SPIFFS.h>
+
+#if defined(NRF52_PLATFORM)
+  #include <InternalFileSystem.h>
+#elif defined(ESP32)
+  #include <SPIFFS.h>
+#endif
 
 #define RADIOLIB_STATIC_ONLY 1
 #include <RadioLib.h>
@@ -11,23 +16,29 @@
 #include <helpers/IdentityStore.h>
 #include <RTClib.h>
 
-/* ------------------------------ Config -------------------------------- */
+#include "SparkFun_Ublox_Arduino_Library.h"
+SFE_UBLOX_GPS myGPS;
 
+/* ---------------------------------- CONFIGURATION ------------------------------------- */
 #ifndef LORA_FREQ
   #define LORA_FREQ   915.0
 #endif
 #ifndef LORA_BW
-  #define LORA_BW     125
+  #define LORA_BW     250
 #endif
 #ifndef LORA_SF
-  #define LORA_SF     9
+  #define LORA_SF     10
 #endif
 #ifndef LORA_CR
   #define LORA_CR      5
 #endif
+#ifndef LORA_TX_POWER
+  #define LORA_TX_POWER  20
+#endif
 
-#ifdef HELTEC_LORA_V3
+#if defined(HELTEC_LORA_V3)
   #include <helpers/HeltecV3Board.h>
+  #include <helpers/CustomSX1262Wrapper.h>
   static HeltecV3Board board;
 #elif defined(ARDUINO_XIAO_ESP32C3)
   #include <helpers/XiaoC3Board.h>
@@ -38,18 +49,28 @@
   #include <helpers/ESP32Board.h>
   #include <helpers/CustomSX1262Wrapper.h>
   static ESP32Board board;
+#elif defined(RAK_4631)
+  #include <helpers/RAK4631Board.h>
+  #include <helpers/CustomSX1262Wrapper.h>
+  static RAK4631Board board;
 #else
   #error "need to provide a 'board' object"
 #endif
 
-#include "SparkFun_Ublox_Arduino_Library.h" //http://librarymanager/All#SparkFun_u-blox_GNSS
-SFE_UBLOX_GPS myGPS;
+// Believe it or not, this std C function is busted on some platforms!
+static uint32_t _atoi(const char* sp) {
+  uint32_t n = 0;
+  while (*sp && *sp >= '0' && *sp <= '9') {
+    n *= 10;
+    n += (*sp++ - '0');
+  }
+  return n;
+}
 
-/* ------------------------------ Code -------------------------------- */
+/* -------------------------------------------------------------------------------------- */
 HardwareSerial gps_serial(1);
 
 class MyMesh : public mesh::Mesh {
-
 public:
   MyMesh(mesh::Radio& radio, mesh::MillisecondClock& ms, mesh::RNG& rng, mesh::RTCClock& rtc, mesh::MeshTables& tables)
      : mesh::Mesh(radio, ms, rng, rtc, *new StaticPoolPacketManager(16), tables)
@@ -57,12 +78,17 @@ public:
   }
 };
 
+#if defined(NRF52_PLATFORM)
+RADIO_CLASS radio = new Module(P_LORA_NSS, P_LORA_DIO_1, P_LORA_RESET, P_LORA_BUSY, SPI);
+#elif defined(P_LORA_SCLK)
 SPIClass spi;
+RADIO_CLASS radio = new Module(P_LORA_NSS, P_LORA_DIO_1, P_LORA_RESET, P_LORA_BUSY, spi);
+#else
+RADIO_CLASS radio = new Module(P_LORA_NSS, P_LORA_DIO_1, P_LORA_RESET, P_LORA_BUSY);
+#endif
 StdRNG fast_rng;
 SimpleMeshTables tables;
-SX1262 radio = new Module(P_LORA_NSS, P_LORA_DIO_1, P_LORA_RESET, P_LORA_BUSY, spi);
-//MyMesh the_mesh(*new RadioLibWrapper(radio, board), *new ArduinoMillis(), fast_rng, *new VolatileRTCClock(), tables);
-MyMesh the_mesh(*new WRAPPER_CLASS(radio, board), fast_rng, *new VolatileRTCClock(), tables);
+MyMesh the_mesh(*new WRAPPER_CLASS(radio, board), *new ArduinoMillis(), fast_rng, *new VolatileRTCClock(), tables);
 
 unsigned long nextAnnounce;
 
@@ -74,39 +100,6 @@ void setup() {
   Serial.begin(115200);
 
   board.begin();
-
-  delay(2000);
-
-  //gps_serial.begin(9600, SERIAL_8N1, D7, D6);
-
-  //Assume that the U-Blox GPS is running at 9600 baud (the default) or at 38400 baud.
-  //Loop until we're in sync and then ensure it's at 38400 baud.
-  // do {
-  //   Serial.println("GPS: trying 38400 baud");
-  //   gps_serial.begin(38400);
-  //   if (myGPS.begin(gps_serial) == true) break;
-
-  //   delay(100);
-  //   Serial.println("GPS: trying 9600 baud");
-  //   gps_serial.begin(9600);
-  //   if (myGPS.begin(gps_serial) == true) {
-  //       Serial.println("GPS: connected at 9600 baud, switching to 38400");
-  //       myGPS.setSerialRate(38400);
-  //       delay(100);
-  //   } else {
-  //       //myGPS.factoryReset();
-  //       delay(2000); //Wait a bit before trying again to limit the Serial output
-  //   }
-  // } while(1);
-  
-  // Serial.println("GPS serial connected");
-
-  // myGPS.setUART1Output(COM_TYPE_UBX); //Set the UART port to output UBX only
-  // myGPS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
-  // myGPS.saveConfiguration(); //Save the current settings to flash and BBR
-
-Serial.println("Initializing radio");
-
 #ifdef SX126X_DIO3_TCXO_VOLTAGE
   float tcxo = SX126X_DIO3_TCXO_VOLTAGE;
 #else
@@ -138,11 +131,43 @@ Serial.println("Initializing radio");
 
   fast_rng.begin(radio.random(0x7FFFFFFF));
 
+  Serial.println("Radio ok");
+
+  gps_serial.begin(38400, SERIAL_8N1, D7, D6);
+
+  // Assume that the U-Blox GPS is running at 9600 baud (the default) or at 38400 baud.
+  // Loop until we're in sync and then ensure it's at 38400 baud.
+  do {
+    Serial.println("GPS: trying 38400 baud");
+    gps_serial.begin(38400);
+    if (myGPS.begin(gps_serial) == true) break;
+
+    delay(100);
+    Serial.println("GPS: trying 9600 baud");
+    gps_serial.begin(9600);
+    if (myGPS.begin(gps_serial) == true) {
+        Serial.println("GPS: connected at 9600 baud, switching to 38400");
+        myGPS.setSerialRate(38400);
+        delay(100);
+    } else {
+        //myGPS.factoryReset();
+        delay(2000); //Wait a bit before trying again to limit the Serial output
+    }
+  } while(1);
+  
+  Serial.println("GPS serial connected");
+
+  myGPS.setUART1Output(COM_TYPE_UBX); //Set the UART port to output UBX only
+  myGPS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
+  myGPS.saveConfiguration(); //Save the current settings to flash and BBR
 
   the_mesh.begin();
   the_mesh.self_id = mesh::LocalIdentity(&fast_rng);  // create new random identity
 
+  Serial.println("Mesh started");
+
   nextAnnounce = 0;
+
 }
 
 void loop() {
@@ -151,7 +176,16 @@ void loop() {
 	  double lat = 4.774;
 	  double lon = -3.385;
 
-    sprintf(data, "LOC %.2f %.2f", lat, lon);
+    long latitude = myGPS.getLatitude();
+    Serial.print(F("Lat: "));
+    Serial.print(latitude);
+
+    long longitude = myGPS.getLongitude();
+    Serial.print(F(" Long: "));
+    Serial.print(longitude);
+    Serial.println(F(" (degrees * 10^-7)"));
+
+    sprintf(data, "LOC %d %d", latitude, longitude);
 
     Serial.print("Sent packet : ");
     Serial.println((char*) data );
@@ -161,8 +195,6 @@ void loop() {
 
     nextAnnounce = the_mesh.futureMillis(30000);  // announce every 30 seconds (test only, don't do in production!)
   }
+
   the_mesh.loop();
-
-
-  // TODO: periodically check for OLD entries in known_clients[], and evict
 }
